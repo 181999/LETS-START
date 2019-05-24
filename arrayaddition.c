3fc1ac012782486a18c1309bafc8471c53181d24
181999 <50978920+181999@users.noreply.github.com>
2019-05-24 17:22:13 +0530

    Create arrayaddition.c

---
+++ arrayaddition.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int input(int a[], int* x)
+{
+        int n,i;
+        printf("Enter the no of elements \n");
+        scanf("%d",x);
+        n=*x;
+        printf("Enter the array elements \n");
+        for(i=0;i<n;i++)
+        {
+                scanf("%d",&a[i]);
+        }
+}
+int add(int a[],int n)
+{
+
+        int sum,i;
+        sum=0;
+        for(i=0;i<n;i++)
+        {
+                sum=sum+a[i];
+        }
+        return sum;
+}
+int output(int p)
+{
+        printf("Sum = %d \n",p);
+
+}
+int main()
+{
+        int a[10],n,s,p;
+        input(a,&n);
+        p=add(a,n);
+        output(p);
+}
