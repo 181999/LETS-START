593afee9fcfa4d5180b4a4a34cc841e547489db7
181999 <50978920+181999@users.noreply.github.com>
2019-05-24 12:21:45 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int input(int a, int b)
+{
+printf("Enter 2 num");
+scanf("%d%d",&a,&b);
+return (a,b);
+}
+int add(int x, int y)
+{
+int s;
+s=x+y;
+}
+int output(int m)
+{
+printf("ans is = %d",m);
+}
+int main()
+{
+int c,d,a;
+input(c,d);
+a=add(c,d);
+output(a);
+}
